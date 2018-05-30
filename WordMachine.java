4528202a38f45f3eabd3c99dbccdde9d5318a030
Filip A. Zofakis <fil.zofakis@gmail.com>
2018-05-30 04:21:12 +0100

    visible nodes in binary tree, word machine (stack)

---
+++ WordMachine.java
@@ -0,0 +1,64 @@
+import java.util.*;
+
+public class FibonacciPartialSum {
+
+    public static int solution(String S) {
+        // write your code in Java SE 8
+        Stack<Integer> st = new Stack<Integer>();
+        String[] split = S.split("\\s+");
+
+        int result = -1;
+
+        try {
+            for (int i = 0; i < split.length; i++) {
+                if (isInteger(split[i], 10)) {
+                    st.push(Integer.parseInt(split[i]));
+                } else {
+                    int value1 = -1;
+                    int value2 = -1;
+
+                    switch (split[i]) {
+                    case "POP":
+                        st.pop();
+                        break;
+                    case "DUP":
+                        value1 = (int) st.peek();
+                        st.push(value1);
+                        break;
+                    case "+":
+                        value1 = Integer.valueOf(st.pop());
+                        value2 = Integer.valueOf(st.pop());
+                        st.push(value1 + value2);
+                        break;
+                    case "-":
+                        value1 = Integer.valueOf(st.pop());
+                        value2 = Integer.valueOf(st.pop());
+                        st.push(value1 - value2);
+                        break;
+                    }
+                }
+            }
+
+            result = Integer.valueOf(st.peek());
+        } catch (Exception e) {
+            return result;
+        }
+
+        return result;
+    }
+
+    public static boolean isInteger(String s, int radix) {
+        Scanner sc = new Scanner(s.trim());
+        if (!sc.hasNextInt(radix))
+            return false;
+        sc.nextInt(radix);
+        return !sc.hasNext();
+    }
+
+    public static void main(String[] args) {
+
+        String s = "13 DUP 4 POP 5 DUP + DUP + -";
+
+        System.out.println(solution(s));
+    }
+}
