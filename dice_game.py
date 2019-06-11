63a00baefec34d88be4a37c36e1dace42cc05eea
Filip A. Zofakis <fil.zofakis@gmail.com>
2019-06-11 22:40:03 +0100

    Dice game with probability comparisons and nested loop iterations

--- dice_game.py
+++ dice_game.py
@@ -1,14 +1,38 @@
-#!/bin/python3
+from random import randint, seed
+from datetime import datetime
+
+seed(datetime.now())
+
+dices = [
+    [1, 1, 6, 6, 8, 8],
+    [2, 2, 4, 4, 9, 9],
+    [3, 3, 5, 5, 7, 7]
+]
+
+def compare_two_dices(dice1, dice2):
+    assert len(dice1) == 6 and len(dice2) == 6
+
+    num_rounds = 10**5
+    num_dice1_wins = 0
+    num_dice2_wins = 0
+
+    for _ in range(num_rounds):
+        dice1_result = dice1[randint(0, 5)]
+        dice2_result = dice2[randint(0, 5)]
+
+        if dice1_result > dice2_result:
+            num_dice1_wins += 1
+        elif dice2_result > dice1_result:
+            num_dice2_wins += 1
+
+    if num_dice1_wins > num_dice2_wins:
+        print("The dice {} is better than {}: \nout of {} rounds it won {} times".format(dice1, dice2, num_rounds, num_dice1_wins))
+    elif num_dice2_wins > num_dice1_wins:
+        print("The dice {} is better than {}: \nout of {} rounds it won {} times".format(dice2, dice1, num_rounds, num_dice2_wins))
+    else:
+        print("A tie")
 
 
-def is_balanced(string_to_check):
-    """Function to check if brackets are balanced with substitution"""
-    brackets = ["()", "{}", "[]"]
-
-    while any(bracket_pair in string_to_check for bracket_pair in brackets):
-        for matched_bracket_pair in brackets:
-            string_to_check = string_to_check.replace(matched_bracket_pair, "")
-    if not string_to_check:
-        return True
-
-    return False
+compare_two_dices(dices[0], dices[1])
+compare_two_dices(dices[1], dices[2])
+compare_two_dices(dices[2], dices[0])
--- iterations_nested_loops.py
+++ iterations_nested_loops.py
@@ -1,4 +1,23 @@
-from itertools import product
+n = 10
+count = 0
 
-for p in product("ab", repeat=4):
-    print("".join(p))
+for i in range(n):
+    for j in range(n):
+        for k in range(n):
+            if i < j < k:
+                count += 1
+
+# 10C3
+print(count)
+
+n = 1000
+count = 0
+
+for i in range(n):
+    for j in range(n):
+        for k in range(n):
+            if i < j < k:
+                count += 1
+
+# 1000C3
+print(count)
