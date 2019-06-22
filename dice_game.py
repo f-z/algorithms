51bef46914d481526b6479059171c881ae30d65f
Filip A. Zofakis <fil.zofakis@gmail.com>
2019-06-22 21:01:51 +0100

    Mutual information

--- dice_game.py
+++ dice_game.py
@@ -1,5 +1,6 @@
-from random import randint, seed
 from datetime import datetime
+import itertools
+from random import randint, seed
 
 seed(datetime.now())
 
@@ -8,6 +9,7 @@ dices = [
     [2, 2, 4, 4, 9, 9],
     [3, 3, 5, 5, 7, 7]
 ]
+
 
 def compare_two_dices(dice1, dice2):
     assert len(dice1) == 6 and len(dice2) == 6
@@ -36,3 +38,23 @@ def compare_two_dices(dice1, dice2):
 compare_two_dices(dices[0], dices[1])
 compare_two_dices(dices[1], dices[2])
 compare_two_dices(dices[2], dices[0])
+
+
+def count_wins(dice1, dice2):
+    assert len(dice1) == 6 and len(dice2) == 6
+    dice1_wins, dice2_wins = 0, 0
+
+    combinations = list(itertools.product(dice1, dice2))
+    for combo in combinations:
+        if combo[0] > combo[1]:
+            dice1_wins += 1
+        elif combo[1] > combo[0]:
+            dice2_wins += 1
+
+    return dice1_wins, dice2_wins
+
+
+print(count_wins([1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6]))
+
+from itertools import permutations
+print(permutations("123456789", 4).__sizeof__())
---
+++ mutual_information.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+
+def mutual_information(arr):
+    pi, pj = marginals(arr)
+
+    h_i = shannon_entropy(pi)
+    h_j = shannon_entropy(pj)
+    h_ij = shannon_entropy(arr)
+
+    mi = h_i + h_j - h_ij
+    return  mi
+
+
+def shannon_entropy(a):
+    a = a[np.nonzero(a)]
+    return -np.sum(a*np.log(a))
+
+
+def marginals(a):
+    marginal_sums = []
+    ranged = list(range(a.ndim))
+    for k in ranged:
+        marginal = np.apply_over_axes(np.sum, a, [j for j in ranged if j != k])
+        marginal_sums.append(marginal)
+    return marginal_sums
