b693c91921f5d384613cd7ceee83959423c84db4
Filip A. Zofakis <fil.zofakis@gmail.com>
2019-06-25 03:24:47 +0100

    Combinations, permutations

--- combinations_permutations.py
+++ combinations_permutations.py
@@ -14,3 +14,23 @@ print(len(list(combinations_with_replacement(range(10), 3))))
 from itertools import combinations_with_replacement
 for c in combinations_with_replacement("TBL", 4):
     print("".join(c))
+
+# Ordered (permutations), without repetitions
+from itertools import permutations
+for a in permutations("123456789", 4):
+    print("".join(a))
+
+print(len(list(permutations("123456789", 4))))
+
+# Permutations with repetitions
+x = [1, 2, 3, 4]
+print(len([p for p in itertools.product(x, repeat=9)]))
+
+import itertools as it
+list_positive_integers_fixed_sum = []
+# Combinations with repetitions
+for d in it.product(range(10), repeat=4):
+    if sum(d) == 10:
+        list_positive_integers_fixed_sum.append(d)
+
+print(len(list_positive_integers_fixed_sum))
